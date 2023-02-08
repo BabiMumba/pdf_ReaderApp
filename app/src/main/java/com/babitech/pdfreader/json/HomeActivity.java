867148b02e510cf23c716f472ede94c614a4557b
Babi Mumba <babimumba243@gmail.com>
2023-02-08 12:03:20 +0200

    add new file

--- app/src/main/java/com/babitech/pdfreader/json/HomeActivity.java
+++ app/src/main/java/com/babitech/pdfreader/json/HomeActivity.java
@@ -1,11 +1,22 @@
 package com.babitech.pdfreader.json;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import com.babitech.pdfreader.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +33,70 @@ public  class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerAdapter(this, viewItems);
+        mRecyclerView.setAdapter(mAdapter);
+
+        addItemsFromJSON();
+
+
+    }
+
+    private void addItemsFromJSON() {
+
+        try {
+
+            String jsonDataString = readJSONDataFromFile();
+            JSONArray jsonArray = new JSONArray(jsonDataString);
+
+            for (int i=0; i<jsonArray.length(); ++i) {
+
+                JSONObject itemObj = jsonArray.getJSONObject(i);
+
+                String name = itemObj.getString("name");
+                String date = itemObj.getString("date");
+
+                Holidays holidays = new Holidays(name, date);
+                viewItems.add(holidays);
+            }
+
+        } catch (IOException | JSONException e) {
+            Log.d(TAG, "addItemsFromJSON: ", e);
+        }
+    }
+
+    private String readJSONDataFromFile() throws IOException{
+
+        InputStream inputStream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+
+            String jsonString = null;
+            inputStream = getResources().openRawResource(R.raw.day);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, "UTF-8"));
+
+            while ((jsonString = bufferedReader.readLine()) != null) {
+                builder.append(jsonString);
+            }
+
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return new String(builder);
     }
 }
